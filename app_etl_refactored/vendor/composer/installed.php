ce80a2dace517401be92b4d07f162397f79b9cf4
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2024-07-31 23:32:00 -0300

    Refactoring do Projeto - Aplicando o princípio na prática

---
+++ app_etl_refactored/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'programador/app_etl',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'aeca3c7050b29ea2fef451ba01577c2a486b6bbe',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'programador/app_etl' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'aeca3c7050b29ea2fef451ba01577c2a486b6bbe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
