904b1db11d402264f8a2294be894cfda7b8c0207
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2024-07-31 21:21:30 -0300

    Lendo um arquivo TXT

--- app_etl/index.php
+++ app_etl/index.php
@@ -4,9 +4,28 @@ require __dir__."/vendor/autoload.php";
 
 use src\Leitor;
 
-$leitor = new Leitor();
-$leitor->setDiretorio(__DIR__.'/arquivos');
-$leitor->setArquivo('dados.csv');
+// Leitor TXT
+$leitorTXT = new Leitor();
+$leitorTXT->setDiretorio(__DIR__.'/arquivos');
+$leitorTXT->setArquivo('dados.txt');
+
+$array_txt = $leitorTXT->lerArquivo();
 echo '<pre>';
-print_r($leitor->lerArquivo());
+print_r($array_txt);
+echo '</pre>';
+
+// Leitor CSV
+$leitorCSV = new Leitor();
+$leitorCSV->setDiretorio(__DIR__.'/arquivos');
+$leitorCSV->setArquivo('dados.csv');
+$array_csv = $leitorCSV->lerArquivo();
+echo '<pre>';
+print_r($array_csv);
+echo '</pre>';
+
+echo '<br>';
+
+// Merge de CSV e TXT
+echo '<pre>';
+print_r(array_merge($array_txt, $array_csv));
 echo '</pre>';
--- app_etl/src/Arquivo.php
+++ app_etl/src/Arquivo.php
@@ -29,4 +29,22 @@ class Arquivo {
             $this->setDados($linha[0], $linha[1], $linha[2]);
         }
     }
+
+    public function lerArquivoTXT(string $caminho): void {
+
+        $handle = fopen($caminho, 'r');
+
+        while(!feof($handle)){
+
+            $linha = fgets($handle);
+
+            $this->setDados(
+                substr($linha, 11, 30), 
+                substr($linha, 0, 11), 
+                substr($linha, 41, 90)
+            );
+        }
+
+        fclose($handle);
+    }
 }
