9339767ddafd7158b27c9b03100ed550d5575584
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2024-08-06 14:08:52 -0300

    Retangulo e quadrado

--- app_poligonos/index.php
+++ app_poligonos/index.php
@@ -1,4 +1,18 @@
 <?php
 
-    require __DIR__.'/vendor/autoload.php';
-    echo 'funcionando';
+require __DIR__.'/vendor/autoload.php';
+
+use src\Retangulo;
+use src\Quadrado;
+
+$retangulo = new Retangulo;
+$retangulo->setAltura(5);
+$retangulo->setLargura(10);
+
+echo '<h3>Área do retângulo: '.$retangulo->getArea().'</h3>';
+
+$quadrado = new Quadrado;
+$quadrado->setAltura(5);
+// $quadrado->setLargura(10);
+
+echo '<h3>Área do quadrado: '.$quadrado->getArea().'</h3>';
