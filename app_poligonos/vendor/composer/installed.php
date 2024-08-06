b7dbc3e4e56350c60ffd4dad6c630baa01563d2e
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2024-08-06 13:37:50 -0300

    Iniciando projeto polígonos

---
+++ app_poligonos/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'programador/app_poligonos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ce80a2dace517401be92b4d07f162397f79b9cf4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'programador/app_poligonos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ce80a2dace517401be92b4d07f162397f79b9cf4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
