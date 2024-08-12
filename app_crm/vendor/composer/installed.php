c5aea6a638a737fa2d29867b87db8c0a41f2fed4
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2024-08-12 20:47:26 -0300

    Iniciando projeto CRM

---
+++ app_crm/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'programador/app_crm',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a1d40678d2a690c928aa6f41a5512a685be5ae2e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'programador/app_crm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a1d40678d2a690c928aa6f41a5512a685be5ae2e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
