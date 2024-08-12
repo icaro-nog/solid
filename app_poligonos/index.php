a1d40678d2a690c928aa6f41a5512a685be5ae2e
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2024-08-12 20:32:06 -0300

    Refactoring do projeto - princípio na prática

--- app_poligonos/index.php
+++ app_poligonos/index.php
@@ -2,17 +2,39 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use src\Retangulo;
-use src\Quadrado;
+use src\Poligono;
+use src\poligonos\Retangulo;
+use src\poligonos\Quadrado;
 
-$retangulo = new Retangulo;
-$retangulo->setAltura(5);
-$retangulo->setLargura(10);
+$poligono = new Poligono;
 
-echo '<h3>Área do retângulo: '.$retangulo->getArea().'</h3>';
+$poligono->setForma(new Retangulo());
+$poligono->getForma()->setAltura(5);
+$poligono->getForma()->setLargura(10);
 
-$quadrado = new Quadrado;
-$quadrado->setAltura(5);
-// $quadrado->setLargura(10);
+echo '<pre>';
+print_r($poligono);
+echo '</pre>';
+echo '<h3>Área do retângulo: '.$poligono->getArea().'</h3>';
 
-echo '<h3>Área do quadrado: '.$quadrado->getArea().'</h3>';
+$poligono->setForma(new Quadrado());
+// $poligono->getForma()->setAltura(5);
+$poligono->getForma()->setLargura(10);
+
+echo '<pre>';
+print_r($poligono);
+echo '</pre>';
+echo '<h3>Área do quadrado: '.$poligono->getArea().'</h3>';
+
+// $retangulo = new Retangulo;
+// $retangulo->setAltura(5);
+// $retangulo->setLargura(10);
+
+// echo '<h3>Área do retângulo: '.$retangulo->getArea().'</h3>';
+
+// $quadrado = new Quadrado;
+// $quadrado->setAltura(5);
+// // $quadrado->setLargura(10);
+
+// echo '<h3>Área do quadrado: '.$quadrado->getArea().'</h3>';
+
--- app_poligonos/src/poligonos/Quadrado.php
+++ app_poligonos/src/poligonos/Quadrado.php
@@ -1,8 +1,12 @@
 <?php
 
-namespace src;
+namespace src\poligonos;
 
-class Quadrado extends Retangulo {
+class Quadrado {
+
+    protected $largura;
+    protected $altura;
+
     public function setLargura(float $largura): void{
         $this->largura = $largura;
         $this->altura = $largura;
@@ -11,5 +15,13 @@ class Quadrado extends Retangulo {
     public function setAltura(float $altura): void{
         $this->altura = $altura;
         $this->largura = $altura;
+    }
+
+    public function getLargura() : float {
+        return $this->largura;
+    }
+
+    public function getAltura() : float {
+        return $this->altura;
     }
 }
