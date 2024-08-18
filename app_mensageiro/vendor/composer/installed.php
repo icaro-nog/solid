d766d3ca6e78b4a51676b278a664c60f2f72b6f7
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2024-08-18 12:24:38 -0300

    Iniciando Projeto Mensageiro

---
+++ app_mensageiro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'programador/app_mensageiro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'acde700219bf6c361063682c1f0fd305930e6ebf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'programador/app_mensageiro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'acde700219bf6c361063682c1f0fd305930e6ebf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
